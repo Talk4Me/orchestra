a1e00159ec0686b6df5b500754cb4905b0697e23
ericslater <ericalexanderslater@gmail.com>
2017-05-13 20:19:40 -0400

    Not drugs.

--- src/client/components/Home.jsx
+++ src/client/components/Home.jsx
@@ -3,10 +3,75 @@ import React from 'react';
 import Title from './Title';
 import ConversationList from './ConversationList';
 import Chat from './Chat';
+import PubNub from 'pubnub';
 
 export default class Home extends React.Component {
   constructor (props) {
     super(props);
+
+
+
+this.pubnub = new PubNub({
+    publishKey: "pub-c-76e8488a-fde8-47c9-ae8a-b16895b941a1",
+    subscribeKey: "sub-c-ab4c26e4-3813-11e7-887b-02ee2ddab7fe",
+    ssl: true
+})
+
+this.pubnub.subscribe({
+    channels: ['my_channel'],
+    withPresence: true 
+})
+
+this.pubnub.addListener({
+    
+    message: function(m) {
+        var channelName = m.channel;
+        var channelGroup = m.subscription; 
+        var msg = m.message; 
+        var pubTT = m.timetoken;
+        console.log("m", m);
+    },
+    presence: function(p) {
+        var action = p.action; 
+        var channelName = p.channel;
+        var occupancy = p.occupancy; 
+        var state = p.state; 
+        var channelGroup = p.subscription; 
+        var publishTime = p.timestamp;
+        var timetoken = p.timetoken;
+        var uuid = p.uuid;
+        console.log("p", p);
+    },
+    status: function(s) {
+        console.log("s", s);
+    }
+})
+
+this.pubnub.publish(
+    {
+        message: { 
+            such: 'object'
+        },
+        channel: 'my_channel',
+        sendByPost: false,
+        storeInHistory: false,
+        meta: { 
+            "cool": "meta"
+        }
+    }, 
+    function (status, response) {
+        if (status.error) {
+            // handle error
+            console.log(status)
+        } else {
+            console.log("message Published w/ timetoken", response.timetoken)
+        }
+    }
+);
+
+
+
+
     const request = new Request('/api/hello', {
       headers: new Headers({
         'Content-Type': 'text/plain'
--- src/server/api.js
+++ src/server/api.js
@@ -1,4 +1,75 @@
 'use strict';
+var PubNub = require('pubnub')
+
+var pubnub = new PubNub({
+    publishKey: "pub-c-76e8488a-fde8-47c9-ae8a-b16895b941a1",
+    subscribeKey: "sub-c-ab4c26e4-3813-11e7-887b-02ee2ddab7fe",
+    ssl: true
+})
+
+pubnub.subscribe({
+    channels: ['my_channel'],
+    withPresence: true 
+})
+
+pubnub.addListener({
+    
+    message: function(m) {
+        var channelName = m.channel;
+        var channelGroup = m.subscription; 
+        var msg = m.message; 
+        var pubTT = m.timetoken;
+        console.log("m", m);
+    },
+    presence: function(p) {
+        var action = p.action; 
+        var channelName = p.channel;
+        var occupancy = p.occupancy; 
+        var state = p.state; 
+        var channelGroup = p.subscription; 
+        var publishTime = p.timestamp;
+        var timetoken = p.timetoken;
+        var uuid = p.uuid;
+        console.log("p", p);
+    },
+    status: function(s) {
+        console.log("s", s);
+    }
+})
+
+pubnub.publish(
+    {
+        message: { 
+            such: 'object'
+        },
+        channel: 'my_channel',
+        sendByPost: false,
+        storeInHistory: false,
+        meta: { 
+            "cool": "meta"
+        }
+    }, 
+    function (status, response) {
+        if (status.error) {
+            // handle error
+            console.log(status)
+        } else {
+            console.log("message Published w/ timetoken", response.timetoken)
+        }
+    }
+);
+
+module.exports = [
+  {
+    method: 'GET',
+    path: '/api/sendMessage',
+    handler: function (request, reply) {
+      const response = 'hello world';
+      pubnub.publish();
+      reply(response);
+    }
+  }
+];
 
 module.exports = [
   {
